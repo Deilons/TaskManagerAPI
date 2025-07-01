4c25b0a28f36fee05f9ff0b2c1138f5d06dfc3cd
Deilons <apolo4748@email.com>
2025-07-01 13:57:28 -0500

    Add project files.

---
+++ TaskManagerAPI/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagerAPI.Models
+{
+    public class Task
+    { 
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; } // high, medium, low
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public bool IsCompleted { get; set; }
+         
+    }
+}
